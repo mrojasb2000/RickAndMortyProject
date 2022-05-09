c5f00d98215d5be9bde300c1af545010bcf2fcc5
Mauricio Rojas <mrojase@vitamina.com>
2022-05-08 20:31:47 -0400

    Config webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Rick and Marty');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+    </>
+  );
+}
+
+const container = document.getElementById('app');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
