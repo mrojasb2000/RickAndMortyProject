5058b55c13b18ae5f190d027586508d49bdcc85e
Mauricio Rojas <mrojase@vitamina.com>
2022-05-11 14:28:34 -0400

    Refactor passing data to component Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,19 @@
-const Character = () => {
-    return <h3>Character</h3>
-};
-
-export default Character;
+function Character(character) {
+    return (
+      <div className='col-3'>
+        <div className='card'>
+          <img
+            src={character.image}
+            alt={character.name}
+            className='card-img-top'
+          />
+          <div className='card-body'>
+            <h3 className='card-title'>{character.name}</h3>
+            <p>{`Origin: ${character.origin && character.origin.name}`}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Character;
